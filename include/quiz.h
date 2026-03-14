7b4bd77c93a6ccaf75f0e539dbb0315bf742bca9
Suresh Reddy <sureshreddy63@yahoo.com>
2026-03-14 12:04:15 +0530

    Create quiz.h

---
+++ include/quiz.h
@@ -0,0 +1,10 @@
+#ifndef QUIZ_H
+#define QUIZ_H
+
+void run_quiz_game(void);
+void show_leaderboard_screen(void);
+void show_help_screen(void);
+void show_about_screen(void);
+void reset_leaderboard_screen(void);
+
+#endif
